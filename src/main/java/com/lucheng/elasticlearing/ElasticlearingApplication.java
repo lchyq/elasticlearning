526718c7be1cf5b4bc646a472ce00dad9af07c25
lucheng28 <lucheng28@jd.com>
2019-12-16 17:10:42 +0800

    first commit

---
+++ src/main/java/com/lucheng/elasticlearing/ElasticlearingApplication.java
@@ -0,0 +1,13 @@
+package com.lucheng.elasticlearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticlearingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticlearingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucheng/elasticlearing/ElasticlearingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucheng.elasticlearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticlearingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
